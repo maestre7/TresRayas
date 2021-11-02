ebff79cfb278cc593eb72227d18f253a306fe0ce
maestre7 <maestre7@gmail.com>
2021-11-02 21:27:46 +0100

    me cago en marcos

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 ﻿//js
+
+let myArray = [];
+
+
+function Square() {
+	let newButton = document.createElement("button");
+	let myAttribute = document.createAttribute("class");
+	myAttribute.value = "square";
+	newButton.setAttributeNode(myAttribute);
+	
+	return newButton;
+}
+
+function Model() {
+	let elementMain = document.querySelector("main");
+	
+	for (controlRow=0; controlRow != 9; controlRow++) {
+		elementMain.appendChild(Square());
+	}
+}
