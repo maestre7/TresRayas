dafbdf78154c10b7431fb1f1f60d6d0c6c646fbf
maestre7 <maestre7@gmail.com>
2021-11-09 11:54:32 +0100

    css and js settings

--- index.js
+++ index.js
@@ -10,13 +10,24 @@ let counter = 0;
 let gameStatus = ["","","","","","","","",""];
 
 // miguel se quejara de lo larga que es... y yo me tendre q morder para no decir mi...
-const winArray = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]];
+const winArray = [[0, 1, 2],
+[3, 4, 5],
+[6, 7, 8],
+[0, 3, 6],
+[1, 4, 7],
+[2, 5, 8],
+[0, 4, 8],
+[2, 4, 6]];
 
 //*********
 // Funciones
 //*********
 
-// funcion para guardar la posicion marcada
+/**
+  * Verifies if the string is in a valid email format
+  * @param email {string}
+  * @return  {boolean}
+  */
 const markPosition = (p) => gameStatus[p] = isTurn ? 'x' : 'o';
 
 
@@ -33,12 +44,30 @@ function Square() {
 	return newButton;
 }
 
+function Indicator(newDivContent1) {
+	let newDivX = document.createElement("div");
+	let newDivO = document.createElement("div");
+	let myIdX = document.createAttribute("id");
+	let myIdO = document.createAttribute("id");
+	myIdX.value = "X";
+	myIdO.value = "O";
+	newDivX.innerHTML = "X";
+	newDivO.innerHTML = "O";
+	newDivX.setAttributeNode(myIdX);
+	newDivO.setAttributeNode(myIdO);
+	newDivContent1.appendChild(newDivX);
+	newDivContent1.appendChild(newDivO);
+	
+	return newDivContent1
+}
+
 // funcion para añadir tres filas de button
 function Model() {
 	let elementMain = document.querySelector("main");
 	
 	let newDivContent1 = document.createElement("div");
-	elementMain.appendChild(newDivContent1);
+	const newDivContent1Padding = Indicator(newDivContent1);
+	elementMain.appendChild(newDivContent1Padding);
 	
 	for (let controlRow=0; controlRow != 3; controlRow++) {
 		let newDiv = document.createElement("div");
@@ -50,6 +79,24 @@ function Model() {
 	}
 	let newDivContent2 = document.createElement("div");
 	elementMain.appendChild(newDivContent2);
+}
+
+function controlTurn(isTurn){
+	if (isTurn) {
+		document.getElementById('X').style.border = 'outset';
+		document.getElementById('O').style.border = 'solid';
+		document.getElementById('X').style.borderWidth = '7px';
+		document.getElementById('O').style.borderWidth = '5px';
+		document.getElementById('X').style.borderColor = '#70757a';
+		document.getElementById('O').style.borderColor = '#70757a'; 
+	} else {
+		document.getElementById('O').style.border = 'outset';
+		document.getElementById('X').style.border = 'solid';
+		document.getElementById('O').style.borderWidth = '7px';
+		document.getElementById('X').style.borderWidth = '5px';
+		document.getElementById('O').style.borderColor = '#70757a';
+		document.getElementById('X').style.borderColor = '#70757a'; 
+	}
 }
 
 // funcion para el marcado
@@ -66,6 +113,8 @@ function mark(myButton){
 		
 		if (winwin == false) {
 			isTurn = isTurn ? false : true; //Cambiamos el turno
+			controlTurn(isTurn);
+			
 		} else if (winwin == true) {
 			alert(`Enorabuena han ganado las ${isTurn ? 'X' : 'O'}`);
 		} else {
